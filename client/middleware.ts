ed9e6d9fcee047555afa79b322bc2650d8ca9b45
MinhThieu145 <nguyen864@usf.edu>
2025-02-18 09:31:48 -0500

    fix: update middleware and packages for clerk

---
+++ client/middleware.ts
@@ -0,0 +1,20 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ["/", "/sign-in", "/sign-up"]
+});
+
+export const config = {
+  // Protects all routes, including api/trpc
+  // You can also enable this to work with static files
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
